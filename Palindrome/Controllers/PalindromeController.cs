4e21cc945d34551dc4d017f7b332deb00c902eea
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-28 09:45:13 -0500

    Add project files.

---
+++ Palindrome/Controllers/PalindromeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Palindrome.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Palindrome.Controllers
+{
+    public class PalindromeController : Controller
+    {
+
+
+        public IActionResult PalindromeView()
+        {
+            var model = new PalindromeModel();
+            return View(model);
+
+        }
+
+        [HttpPost]
+        public IActionResult PalindromeView(PalindromeModel model)
+        {
+
+            var startValue = model.UserInput.Length - 1;
+
+            var endValue = 0;
+
+            var revString = "";
+
+
+
+            for (var i = startValue; i >= endValue; i--)
+            {
+                revString += model.UserInput[i];
+
+            }
+            if (model.UserInput == revString)
+                {
+                    model.Output =  "That's a Palindrome!";
+                }
+                else
+                {
+                    model.Output =  "That's not a Palindrome";
+                }
+            
+            return View(model);
+        }
+
+    }
+}
---
+++ Palindrome/Models/PalindromeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Palindrome.Models
+{
+    public class PalindromeModel
+    {
+        public string UserInput { get; set; }
+        public string ReverseInput { get; set; }
+
+        public string Output { get; set; }
+    }
+
+}
